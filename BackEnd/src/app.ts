c2e45c5fa4fad5382b237332ddba8aaf198e66a2
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-10-09 16:19:41 +0900

    feature : lint 추가

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -4,10 +4,10 @@ const app = express();
 
 const port = 3000;
 
-app.get("/",(req,res) => {
-	res.send("hello,world!\n");	
+app.get('/', (req, res) => {
+  res.send('hello,world!\n');
 });
 
-app.listen(port,() => {
-	console.log("http://localhost:"+port);
-})
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
