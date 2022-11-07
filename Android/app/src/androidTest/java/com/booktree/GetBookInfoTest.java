04b09e24c1f052637eb5de2ce8bd434fc0122cf6
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-07 22:52:17 +0900

    test : test코드 추가

--- Android/app/src/androidTest/java/com/booktree/GetBookInfoTest.java
+++ Android/app/src/androidTest/java/com/booktree/GetBookInfoTest.java
@@ -7,16 +7,43 @@ import com.booktree.API.APIClient;
 import java.io.IOException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(AndroidJUnit4.class)
 public class GetBookInfoTest {
   @Test
   public void isAPIValid() {
     try {
-      var res = APIClient.getInstance().getKakaoAPI().getBookInfo("https://brunch.co.kr/@tourism 집짓기",
+      var res = APIClient.getInstance().getKakaoAPI().getBookInfo("어린 왕자",
         "accuracy",
-        1,10);
-      assertTrue(res.execute().isSuccessful());
+        1,10).execute();
+      assertTrue(res.isSuccessful());
+    } catch (IOException e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+
+  @Test
+  public void isSearchDoWell() {
+    try {
+      var res = APIClient.getInstance().getKakaoAPI().getBookInfo("어린 왕자",
+          "accuracy",
+          1,10).execute();
+      assertThat(res.body().meta.total_count).isGreaterThan(0);
+    } catch (IOException e) {
+      e.printStackTrace();
+      fail();
+    }
+  }
+  @Test
+  public void canSearchWithISBN() {
+    try {
+      var res = APIClient.getInstance().getKakaoAPI().getBookInfo("9791159171772",
+          "accuracy",
+          1,10).execute();
+      assertThat(res.body().meta.total_count).isGreaterThan(0);
+      assertThat(res.body().documents[0].title).isEqualTo("일본어 들어가기");
     } catch (IOException e) {
       e.printStackTrace();
       fail();
