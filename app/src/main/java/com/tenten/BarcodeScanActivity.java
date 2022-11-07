d64448eeb89db32d9bd24b3152b877185e9c5667
youseungjua <yousuengjua@gmail.com>
2022-11-07 21:06:00 +0900

    바코드 스캔 Activity 추가

---
+++ app/src/main/java/com/tenten/BarcodeScanActivity.java
@@ -0,0 +1,39 @@
+package com.tenten;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class BarcodeScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode_scan);
+    }
+
+    public void startBarcodeReader(View view){
+        new IntentIntegrator(this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result!=null){
+            if(result.getContents() != null){ //바코드를 제대로 읽은 경우
+                Toast.makeText(this, "scanned: "+result.getContents(), Toast.LENGTH_LONG).show();
+            }else{ //사용자가 이전으로 가기 원할떄
+                Toast.makeText(this, "cancelled",Toast.LENGTH_LONG).show();
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
