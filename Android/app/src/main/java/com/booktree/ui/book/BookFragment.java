00065969115be57dd7dd6812fa80442c12c3764c
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-17 20:58:12 +0900

    Revert "fix : confict 해결"
    This reverts commit a669f4bae4074a34448e63cc02f165ad2b99929b.

--- Android/app/src/main/java/com/booktree/ui/book/BookFragment.java
+++ Android/app/src/main/java/com/booktree/ui/book/BookFragment.java
@@ -1,6 +1,5 @@
 package com.booktree.ui.book;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -8,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
-import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -16,7 +14,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.booktree.API.APIClient;
 import com.booktree.API.DTO.BookDTO;
-import com.booktree.BookDetailActivity;
 import com.booktree.databinding.FragmentBookBinding;
 import com.booktree.ui.book.bookList.BookRecyclerList;
 import com.booktree.ui.book.bookList.bookListAdapter;
@@ -48,12 +45,6 @@ public class BookFragment extends Fragment {
     var bookInfoList = binding.bookInfoList;
     var list = new BookRecyclerList(bookInfoList,getActivity());
     bookViewModel.getQueryString().observe(getViewLifecycleOwner(), list.getInitialListItems());
-
-    Button barcodeBtn = binding.barcodeBtn;
-    barcodeBtn.setOnClickListener((view) -> {
-      Intent intent = new Intent(getActivity(), BarcodeScanActivity.class);
-      getActivity().startActivity(intent);
-    });
 
     return root;
   }
