a2b6301436ed0d5c116639ec6ce632ebd9a862c3
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-20 22:01:55 +0900

    Refactor : 책 검색 Fragment

--- Android/app/src/main/java/com/booktree/ui/book/bookList/BookListAdapter.java
+++ Android/app/src/main/java/com/booktree/ui/book/bookList/BookListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,12 +14,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.booktree.model.Documents;
 import com.booktree.BookDetailActivity;
 import com.booktree.R;
+import com.booktree.ui.book.bookList.Viewholder.BasicViewHolder;
 import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.jetbrains.annotations.NotNull;
 
-public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.ViewHolder> {
+public abstract class BookListAdapter<T extends BasicViewHolder> extends RecyclerView.Adapter<T> {
 
   private ArrayList<Documents> documents;
   private final Context context;
@@ -38,19 +40,8 @@ public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.ViewHo
     notifyItemRangeRemoved(0,size);
   }
 
-  @NonNull
-  @NotNull
   @Override
-  public BookListAdapter.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent,
-      int viewType) {
-    View view = LayoutInflater.from(parent.getContext())
-        .inflate(R.layout.book_info_item, parent, false);
-
-    return new ViewHolder(view);
-  }
-
-  @Override
-  public void onBindViewHolder(@NonNull @NotNull BookListAdapter.ViewHolder holder, int position) {
+  public void onBindViewHolder(@NonNull @NotNull T holder, int position) {
     holder.setContents(context, documents.get(position));
   }
 
@@ -59,30 +50,4 @@ public class BookListAdapter extends RecyclerView.Adapter<BookListAdapter.ViewHo
     return documents.size();
   }
   
-  public static class ViewHolder extends RecyclerView.ViewHolder {
-    private final TextView titleText;
-    private final TextView contentText;
-    private final ImageView thumbNailContents;
-    private final ConstraintLayout bookInfoLayout;
-
-    public ViewHolder(View view) {
-      super(view);
-      // Define click listener for the ViewHolder's View
-      titleText = view.findViewById(R.id.bookInfoTitle);
-      contentText = view.findViewById(R.id.bookInfoContents);
-      thumbNailContents = view.findViewById(R.id.Thumbnail);
-      bookInfoLayout = view.findViewById(R.id.bookInfoLayout);
-    }
-
-    public void setContents(Context context, Documents item) {
-      titleText.setText(item.title);
-      contentText.setText(item.publisher);
-      Glide.with(context).load(item.thumbnail).into(thumbNailContents);
-      bookInfoLayout.setOnClickListener((View view) -> {
-        Intent intent = new Intent(context, BookDetailActivity.class);
-        intent.putExtra("document", item);
-        context.startActivity(intent);
-      });
-    }
-  }
 }
---
+++ Android/app/src/main/java/com/booktree/ui/book/bookList/Viewholder/BasicViewHolder.java
@@ -0,0 +1,36 @@
+package com.booktree.ui.book.bookList.Viewholder;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import com.booktree.BookDetailActivity;
+import com.booktree.R;
+import com.booktree.model.Documents;
+import com.bumptech.glide.Glide;
+
+public class BasicViewHolder extends RecyclerView.ViewHolder {
+  private final TextView titleText;
+  private final TextView contentText;
+  private final ImageView thumbNailContents;
+  protected final ConstraintLayout bookInfoLayout;
+
+  public BasicViewHolder(View view) {
+    super(view);
+    // Define click listener for the ViewHolder's View
+    titleText = view.findViewById(R.id.bookInfoTitle);
+    contentText = view.findViewById(R.id.bookInfoContents);
+    thumbNailContents = view.findViewById(R.id.Thumbnail);
+    bookInfoLayout = view.findViewById(R.id.bookInfoLayout);
+  }
+
+  public void setContents(Context context, Documents item) {
+    titleText.setText(item.title);
+    contentText.setText(item.publisher);
+    Glide.with(context).load(item.thumbnail).into(thumbNailContents);
+  }
+}
