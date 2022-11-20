dbcad9d3ad38300c8fffcc0cb7d41863dbc687b5
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-20 23:03:20 +0900

    feature : 데이터 가져오기 성공

---
+++ Android/app/src/main/java/com/booktree/ui/feed/feedCreate/FeedCreateViewModel.java
@@ -0,0 +1,22 @@
+package com.booktree.ui.feed.feedCreate;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import com.booktree.model.Documents;
+
+public class FeedCreateViewModel extends ViewModel {
+  private MutableLiveData<Documents> Document;
+
+  public FeedCreateViewModel() {
+    Document = new MutableLiveData<>();
+  }
+
+  public void setDocument(Documents document) {
+    Document.setValue(document);
+  }
+
+  public LiveData<Documents> getDocument() {
+    return Document;
+  }
+}
