3962f111f3582af6f60316ca5175be670adfc3b5
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-22 11:22:18 +0900

    feature : 데이터 가져오기 처리중

---
+++ Android/app/src/androidTest/java/com/booktree/FireBaseTest.java
@@ -0,0 +1,29 @@
+package com.booktree;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import com.booktree.API.FBDatabase;
+import com.booktree.model.Feed;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class FireBaseTest {
+  @Test
+  public void 파이어베이스_연결_테스트() {
+    FBDatabase.getInstance();
+  }
+  @Test
+  public void 피드_생성_테스트() {
+    var feed = new Feed("9791159171772",
+        "testAuthor",
+        "test content",
+        "https://i.stack.imgur.com/GsDIl.jpg");
+    var res =  FBDatabase.getInstance().createFeed(feed);
+    res.onSuccessTask((response) -> {
+    });
+  }
+}
--- Android/app/src/main/java/com/booktree/ui/feed/feedCreate/FeedCreateActivity.java
+++ Android/app/src/main/java/com/booktree/ui/feed/feedCreate/FeedCreateActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build.VERSION;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -18,6 +19,7 @@ import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import com.booktree.databinding.ActivityCreateFeedBinding;
 import com.booktree.model.Documents;
+import com.booktree.ui.book.bookList.Viewholder.BasicViewHolder;
 
 public class FeedCreateActivity extends AppCompatActivity {
 
@@ -32,6 +34,9 @@ public class FeedCreateActivity extends AppCompatActivity {
     viewModel = new ViewModelProvider(this).get(FeedCreateViewModel.class);
     setContentView(binding.getRoot());
     final var selectBookBtn = binding.selectBook;
+    final var selectBookInfo = binding.createFeedBookInfo.getRoot();
+
+    final var viewHolder = new BasicViewHolder(selectBookInfo);
 
     var getResult = registerForActivityResult(new StartActivityForResult(),
         result -> {
@@ -51,6 +56,9 @@ public class FeedCreateActivity extends AppCompatActivity {
     });
 
     viewModel.getDocument().observe(this,(data) -> {
+      binding.selectBookLayout.setVisibility(View.INVISIBLE);
+      binding.createFeedBookInfo.getRoot().setVisibility(View.VISIBLE);
+      viewHolder.setContents(this,data);
       Toast.makeText(this, data.title, Toast.LENGTH_SHORT).show();
     });
   }
