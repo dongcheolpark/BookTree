6bb6a6dd045eb27a41b51950e6f5b5a3e4af0419
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-22 11:43:37 +0900

    refactor : 리펙토링 성공

--- Android/app/src/main/java/com/booktree/ui/book/BookFragment/BookToInfoFragment.java
+++ Android/app/src/main/java/com/booktree/ui/book/BookFragment/BookToInfoFragment.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.booktree.R;
 import com.booktree.ui.book.bookList.BookListAdapter;
+import com.booktree.ui.book.bookList.BookListAdapter.Type;
 import com.booktree.ui.book.bookList.BookRecyclerList;
 import com.booktree.ui.book.bookList.Viewholder.ToBookInfoViewHolder;
 import org.jetbrains.annotations.NotNull;
@@ -15,15 +16,6 @@ public class BookToInfoFragment extends BookFragment {
 
   @Override
   protected BookRecyclerList getList(RecyclerView bookInfoList) {
-    return new BookRecyclerList(bookInfoList,getActivity(),
-        new BookListAdapter<ToBookInfoViewHolder>(getActivity()) {
-          @NonNull
-          @NotNull
-          @Override
-          public ToBookInfoViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent,
-              int viewType) {
-            return new ToBookInfoViewHolder(createView(parent));
-          }
-        });
+    return new BookRecyclerList(bookInfoList,getActivity(), Type.toBook);
   }
 }
