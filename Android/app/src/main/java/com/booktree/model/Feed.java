92c865f9dc9b11d80fc4d61366916860cc9c4d26
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-25 17:16:11 +0900

    feature : 피드 가져오기 성공

--- Android/app/src/main/java/com/booktree/model/Feed.java
+++ Android/app/src/main/java/com/booktree/model/Feed.java
@@ -8,6 +8,8 @@ public class Feed implements Serializable {
   public String contents;
   public String imageUrl;
 
+  public Feed() {}
+
   public Feed(String book,String author,String contents, String ImageUrl) {
     this.book = book;
     this.author = author;
