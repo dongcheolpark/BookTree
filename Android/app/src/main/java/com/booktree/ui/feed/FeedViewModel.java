4e87d439bc8e7e002c359c24e508da75027d6d33
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-25 17:16:11 +0900

    feature : 피드 가져오기 성공

--- Android/app/src/main/java/com/booktree/ui/feed/FeedViewModel.java
+++ Android/app/src/main/java/com/booktree/ui/feed/FeedViewModel.java
@@ -1,8 +1,8 @@
 package com.booktree.ui.feed;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.booktree.API.FBDatabase;
 import com.booktree.common.MutableListLiveData;
 import com.booktree.model.Feed;
 import java.util.ArrayList;
@@ -16,10 +16,10 @@ public class FeedViewModel extends ViewModel {
   }
 
   public void refreshFeedList() {
-    mFeedList.add(new Feed("9791159171772",
-        "testAuthor",
-        "test content",
-        "https://i.stack.imgur.com/GsDIl.jpg"));
+    FBDatabase.getInstance().getFeed((list) -> {
+      mFeedList.clear(false);
+      mFeedList.addAll(list);
+    });
   }
 
   public LiveData<ArrayList<Feed>> getFeedList() {
