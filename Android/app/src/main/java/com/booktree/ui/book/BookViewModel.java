e7d51dc84447b69baf0ea1653d720c1252a81503
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-30 22:02:26 +0900

    design : BookFragment 디자인 변경

--- Android/app/src/main/java/com/booktree/ui/book/BookViewModel.java
+++ Android/app/src/main/java/com/booktree/ui/book/BookViewModel.java
@@ -3,14 +3,23 @@ package com.booktree.ui.book;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.booktree.ui.book.bookList.BookRecyclerList;
 
 public class BookViewModel extends ViewModel {
 
   private MutableLiveData<String> mQueryString;
+  private BookRecyclerList bookInfoList;
 
   public BookViewModel() {
     mQueryString = new MutableLiveData<>();
     mQueryString.setValue("This is book fragment");
+  }
+
+  public void setBookInfoList(BookRecyclerList bookInfoList) {
+    this.bookInfoList = bookInfoList;
+  }
+  public BookRecyclerList getBookInfoList() {
+    return bookInfoList;
   }
 
   public LiveData<String> getQueryString() {
