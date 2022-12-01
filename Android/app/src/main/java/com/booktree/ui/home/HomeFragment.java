92c0b9f9e775a4492ba0635b16e13faf262798a4
youseungjua <yousuengjua@gmail.com>
2022-12-01 12:31:26 +0900

    DB없는 달력

--- Android/app/src/main/java/com/booktree/ui/home/HomeFragment.java
+++ Android/app/src/main/java/com/booktree/ui/home/HomeFragment.java
@@ -4,34 +4,114 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import com.booktree.R;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import com.booktree.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+//  private static final String TAG = "HomeFragment";
+  private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM yyyy", Locale.ENGLISH);
+  private SimpleDateFormat dateFormatForDay = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
 
   private HomeViewModel homeViewModel;
   private FragmentHomeBinding binding;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
-      ViewGroup container, Bundle savedInstanceState) {
+                           ViewGroup container, Bundle savedInstanceState) {
     homeViewModel =
         new ViewModelProvider(this).get(HomeViewModel.class);
 
     binding = FragmentHomeBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-    final TextView textView = binding.textHome;
-    homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    final var compactCalendarView = binding.compactcalendarView;
+    final var textView_month = binding.textViewMonth;
+    final var textView_result = binding.textViewResult;
+
+    textView_month.setText(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth())); //위에 MMM yyyy 표시
+    compactCalendarView.setFirstDayOfWeek(Calendar.SUNDAY);  //일요일을 시작으로
+
+
+    // 날짜 클릭 이벤트 관련 코드
+    compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
       @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
+      public void onDayClick(Date dateClicked) {
+        String date = dateFormatForDay.format(dateClicked); //날짜 나오기
+        textView_result.setText(date);
       }
+
+
+
+      @Override
+      public void onMonthScroll(Date firstDayOfNewMonth) {
+        textView_month.setText(dateFormatForMonth.format(firstDayOfNewMonth));
+      }
+
+
+
+//        // 해당날짜에 이벤트가 있으면
+//        if (events.size() > 0) {
+//          textView_result.setText(events.get(0).getData().toString());
+//        }
+//        // 해당날짜에 이벤트가 없으면
+//        else {
+//          SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-MM-dd", Locale.KOREA);
+//          String clickDate = simpleDate.format(dateClicked);
+//
+//          EditText editText = new EditText(HomeFragment.this.getContext());
+//          AlertDialog.Builder builder = new AlertDialog.Builder(HomeFragment.this.getContext());
+//          AlertDialog dialogText = builder.setTitle("추가할 일정을 입력해 주세요.")
+//                  // .setMessage("메시지 입력")
+//                  .setView(editText)
+//                  .setPositiveButton("저장하기", new DialogInterface.OnClickListener() {
+//                    @SuppressLint("SetTextI18n")
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                      String dateClicked_st = simpleDate.format(dateClicked);
+//                      Log.d(TAG, "태그 dateClicked_st : " + dateClicked_st);
+//
+//                      Date currentDay = null;
+//                      try {
+//                        // .parse 함수 : Parses text from a string to produce a Date (문자열에서 텍스트를 분석하여 날짜 생성)
+//                        currentDay = simpleDate.parse(dateClicked_st);
+//                        Log.d(TAG, "태그 currentDay : " + currentDay);
+//
+//                      } catch (ParseException e) {
+//                        e.printStackTrace();
+//                      }
+//                      Long currentLong = currentDay.getTime();
+//                      Log.d(TAG, "태그 currentLong : " + currentLong);
+//
+//                      Event ev1 = new Event(Color.GREEN, currentLong, clickDate + " : " + editText.getText().toString());
+//                      compactCalendarView.addEvent(ev1);
+//                      textView_result.setText(clickDate + " : " + editText.getText().toString());
+//                      Toast.makeText(HomeFragment.this.getContext(), "일정이 저장되었습니다.", Toast.LENGTH_SHORT).show();
+//                    }
+//                  })
+//                  .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                      // 취소 클릭 시 실행할 거 작성
+//                      Toast.makeText(HomeFragment.this.getContext(), "일정입력 취소되었습니다.", Toast.LENGTH_SHORT).show();
+//                    }
+//                  })
+//                  .create();
+//          dialogText.show();
+//        }
+//      }
+
+//      @Override
+//      public void onMonthScroll(Date firstDayOfNewMonth) {
+//        textView_month.setText(dateFormatForMonth.format(firstDayOfNewMonth));
+//        Log.d(TAG, "Month was scrolled to: " + firstDayOfNewMonth);
+//      }
     });
 
     return root;
