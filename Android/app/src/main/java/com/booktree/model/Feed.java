5cc008a7ae9077afae3c48bb66b2cfbcc87756d9
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-12-01 18:44:59 +0900

    feature : feed 날짜 추가

--- Android/app/src/main/java/com/booktree/model/Feed.java
+++ Android/app/src/main/java/com/booktree/model/Feed.java
@@ -1,19 +1,22 @@
 package com.booktree.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Feed implements Serializable {
   public String book;
   public String author;
   public String contents;
   public String imageUrl;
+  public Date uploadDate;
 
   public Feed() {}
 
-  public Feed(String book,String author,String contents, String ImageUrl) {
+  public Feed(String book,String author,String contents, String ImageUrl,Date uploadDate) {
     this.book = book;
     this.author = author;
     this.contents = contents;
     this.imageUrl = ImageUrl;
+    this.uploadDate = uploadDate;
   }
 }
