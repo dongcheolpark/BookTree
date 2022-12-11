443cfca9b79fb96ad26cae768c51826b9b2d824e
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-12-11 14:27:46 +0900

    feature : 잡다한거

--- Android/app/src/main/java/com/booktree/API/GetDocumentAPI.java
+++ Android/app/src/main/java/com/booktree/API/GetDocumentAPI.java
@@ -1,8 +1,15 @@
 package com.booktree.API;
 
+import android.util.Log;
 import com.booktree.API.DTO.BookDTO;
+import com.booktree.API.FBDatabase.FBCallbackWithArray;
 import com.booktree.common.ResultCallBack;
 import com.booktree.model.Documents;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,5 +40,25 @@ public class GetDocumentAPI {
           }
         }
     );
+  }
+  public void getDocumentsWithISBN(List<String> isbnList, FBCallbackWithArray<Documents> callback) {
+    var size = isbnList.size();
+    var countDownLatch = new CountDownLatch(size);
+    var res = new ArrayList<Documents>();
+    var executorService = Executors.newFixedThreadPool(1);
+    executorService.execute(() -> {
+        isbnList.forEach((isbn) -> {
+            getDocumentWithISBN(isbn,(book) -> {
+              res.add(book);
+              countDownLatch.countDown();
+            });
+        });
+        try {
+          countDownLatch.await(5, TimeUnit.SECONDS);
+          callback.onGetSuccess(res);
+        } catch (InterruptedException e) {
+          Log.e("ERROR : ", e.getMessage());
+        }
+      });
   }
 }
--- Android/app/src/main/java/com/booktree/ui/book/bookFragment/ListFragment/RecommendFragment.java
+++ Android/app/src/main/java/com/booktree/ui/book/bookFragment/ListFragment/RecommendFragment.java
@@ -1,6 +1,7 @@
 package com.booktree.ui.book.bookFragment.ListFragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,5 +39,11 @@ public class RecommendFragment extends Fragment {
       viewModel.getRecommendList().getAdapter()
           .setList(list);
     });
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    Log.i("info", "onDestroy: ");
   }
 }
